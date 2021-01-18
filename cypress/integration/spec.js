d9e5d62e96c9f706d8b6fb9d987ddf7e46ff5d6d
Josip Bojcic <jbojcic1@gmail.com>
2021-01-18 13:46:11 +0100

    Before unload issue example

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,17 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+describe('before page unload', () => {
+  it('styles on the page when canceled', () => {
+    // https://codesandbox.io/s/modern-mountain-1vd73?file=/index.html
+    cy.visit('https://1vd73.csb.app/');
+    cy.get('input').type('some txt').then(() => {
+      debugger;
+      cy.on('window:before:unload', (event) => {
+        debugger;
+      });
+      cy.on('window:unload', (event) => {
+        debugger;
+      });
+    });
+    cy.reload();
   })
 })
